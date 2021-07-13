837c98538439f88710d89d29bb5f3f32e541c9bd
AliMarangoz <ali.marangoz1999@gmail.com>
2021-07-13 17:40:10 +0300

    Pre-message fixed.

--- php/data.php
+++ php/data.php
@@ -1,12 +1,27 @@
 <?php
 
 while ($row = mysqli_fetch_assoc($sql)){
+    $sql2 = "select * from messages where (incoming_msg_id = {$row['unique_id']}
+    or outgoing_msg_id = {$row["unique_id"]}) and (outgoing_msg_id = {$outgoing_id}
+    or outgoing_msg_id = {$outgoing_id}) order by msg desc limit 1";
+
+    $query2 = mysqli_query($conn,$sql2);
+    $row2 = mysqli_fetch_assoc($query2);
+    if(mysqli_num_rows($query2) > 0 ){
+        $result = $row2["msg"];
+    }else{
+        $result = "No message available";
+    }
+
+    (strlen($result) > 28) ? $msg = substr($result,0,28).'...' : $msg = $result; //trimming message if word are more than 28
+    ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+
     $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
                     <div class="content">
                         <img src="php/image/'.$row['img'].'">
                         <div class="details">
                             <span>'.$row['fname']. " ". $row['lname'].'</span>
-                            <p>Test Message</p>
+                            <p>'.$you.$msg.'</p>
                         </div>
                     </div>
                     <div class="status-dot">
--- php/users.php
+++ php/users.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     require_once "config.php";
+    $outgoing_id = $_SESSION["unique_id"];
     $sql = mysqli_query($conn,"select * from users");
     $output = "";
     if(mysqli_num_rows($sql) == 1){
