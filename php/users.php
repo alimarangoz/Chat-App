82c68889a44fb0f5b637b1eaf461eb373bc07c8d
AliMarangoz <ali.marangoz1999@gmail.com>
2021-07-14 09:33:01 +0300

    Bugs fixed-Logout implemented-Status implemented

--- php/users.php
+++ php/users.php
@@ -2,7 +2,7 @@
     session_start();
     require_once "config.php";
     $outgoing_id = $_SESSION["unique_id"];
-    $sql = mysqli_query($conn,"select * from users");
+    $sql = mysqli_query($conn,"select * from users where not unique_id = {$outgoing_id}");
     $output = "";
     if(mysqli_num_rows($sql) == 1){
         $output = "No users are available to chat";
